06d13515f171ae558e0dc34948f330932243693a
leonardo3130 <leonardopo313@gmail.com>
2024-05-17 01:50:34 +0200

    es2 finito, da testare

--- Exams/20240214/es2.c
+++ Exams/20240214/es2.c
@@ -11,13 +11,14 @@ per 3 volte. Nella prima esecuzione la working directory deve essere la dir corr
 deve avere come working directory './dir1' e la terza './dir2/dir3'.
 */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #define MAX_BUF_SIZE 1024
 
 void run_name(char *file_name, char *path, char *arguments[]) {
@@ -34,18 +35,29 @@ void run_name(char *file_name, char *path, char *arguments[]) {
   }
 
   while ((directory_entry = readdir(dir)) != NULL) {
-    //printf("%s\n", directory_entry->d_name);
     if(strcmp(directory_entry->d_name, ".") == 0 || strcmp(directory_entry->d_name, "..") == 0)
       continue;
     
     snprintf(new_path, sizeof(new_path), "%s/%s",path, directory_entry->d_name);
     if(directory_entry->d_type == DT_DIR) {
-      run_name(file_name, new_path);
+      run_name(file_name, new_path, arguments);
     } else {
       if(strcmp(file_name, directory_entry->d_name) == 0 && access(new_path, X_OK) == 0) { //controlla che new_path abbia permesso da eseguibile
         //esegui l'eseguibile
         //cwd = path !!
-
+        pid_t fork_retvalue;
+        switch (fork_retvalue = fork()) {
+          case 0:
+            execvp(new_path, arguments);
+            break;
+          default:
+            int status;
+			      waitpid(fork_retvalue, &status, 0); //aspetta la fine
+            break;
+          case -1:
+            perror("fork");
+            break;
+        }
       }
     }
   }
@@ -54,6 +66,6 @@ void run_name(char *file_name, char *path, char *arguments[]) {
 
 int main(int argc, char *argv[])
 {
-  run_name(argv[1], ".", argv);
+  run_name(argv[1], ".", argv + 1);
   return EXIT_SUCCESS;
 }
